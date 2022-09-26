905d455600d98d9c2feef13a3bd4318ccbfeef7b
srikanthpragada <srikanthpragada@gmail.com>
2022-09-26 17:58:10 +0530

    26-SEP

--- springdemo/src/main/java/jdbc/JobsManager.java
+++ springdemo/src/main/java/jdbc/JobsManager.java
@@ -18,49 +18,25 @@ import org.springframework.transaction.interceptor.TransactionAspectSupport;
 public class JobsManager  {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-
-	public void showAverageSalary() {
-		Double avgSalary = jdbcTemplate.queryForObject
-				 ("select round(avg(salary)) from employees", Double.class);
-		System.out.println("Avgerage salary  = " + avgSalary);
-	}
 	
 	public void showJobCount() {
 		Integer count = jdbcTemplate.queryForObject("select count(*) from jobs", Integer.class);
 		System.out.println("Count = " + count);
 	}
 
-	public void listJobsWith7() {
-		List<Job> jobs = jdbcTemplate.query("select * from jobs", 
-			new RowMapper<Job>() {
-		    	@Override
-			    public Job mapRow(ResultSet rs, int rowNum) throws SQLException {
-				    return  new Job(rs.getString("id"),rs.getString("title"));
-			    }
-		    });
-
-		for (Job j : jobs)
-			System.out.println(j.getTitle());
+	public void showAverageSalary() {
+		Double avgSalary = jdbcTemplate.queryForObject
+				 ("select round(avg(salary)) from employees", Double.class);
+		System.out.println("Avgerage salary  = " + avgSalary);
 	}
 
-	public void listJobsWithLambda() {
-		List<Job> jobs = jdbcTemplate.query("select * from jobs",
-				// RowMapper (mapRow()) is implemented using Lambda Expression
-				(rs, rowNum) -> new Job(rs.getString("id"), rs.getString("title")));
-
-		for (Job j : jobs)
-			System.out.printf("%-10s  %s\n",j.getId(), j.getTitle());
+	public void updateMinSalary() {
+		int count = jdbcTemplate.update
+				("update jobs set minsal = ? where id = ?", 500000, "apro");
+		System.out.println("No. of rows updated : " + count);
 	}
-
-	public void listJobTitles() {
-		List<String> titles = jdbcTemplate.query("select title from jobs",
-				// RowMapper is implemented using Lambda Expression
-				(rs, rowNum) -> rs.getString("title"));
-
-		for (String title : titles)
-			System.out.println(title);
-	}
-
+	
+	
 	public void listJobTitlesWithJava7() {
 		List<String> titles = jdbcTemplate.query("select title from jobs",
 	     // Anonymous inner class to implement RowMapper		
@@ -78,11 +54,39 @@ public class JobsManager  {
 			System.out.println(title);
 	}
 
-	public void updateMinSalary() {
-		int count = jdbcTemplate.update
-				("update jobs set minsal = ? where id = ?", 500000, "jpro");
-		System.out.println("No. of rows updated : " + count);
+	public void listJobTitles() {
+		List<String> titles = jdbcTemplate.query("select title from jobs",
+				// RowMapper is implemented using Lambda Expression
+				(rs, rowNum) -> rs.getString("title"));
+
+		for (String title : titles)
+			System.out.println(title);
 	}
+
+	
+	public void listJobsWith7() {
+		List<Job> jobs = jdbcTemplate.query("select * from jobs", 
+			new RowMapper<Job>() {
+		    	@Override
+			    public Job mapRow(ResultSet rs, int rowNum) throws SQLException {
+				    return  new Job(rs.getString("id"),rs.getString("title"));
+			    }
+		    });
+
+		for (Job j : jobs)
+			System.out.println(j.getId() + " " + j.getTitle());
+	}
+
+	public void listJobsWithLambda() {
+		List<Job> jobs = jdbcTemplate.query("select * from jobs",
+				// RowMapper (mapRow()) is implemented using Lambda Expression
+				(rs, rowNum) -> new Job(rs.getString("id"), rs.getString("title")));
+
+		for (Job j : jobs)
+			System.out.printf("%-10s  %s\n",j.getId(), j.getTitle());
+	}
+
+	
 
 	@Transactional(propagation = Propagation.REQUIRED)
 	public void updateTwo(int e1, int e2) {
--- springdemo/src/main/java/jdbc/TestJDBC.java
+++ springdemo/src/main/java/jdbc/TestJDBC.java
@@ -9,15 +9,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @SpringBootApplication
 public class TestJDBC implements CommandLineRunner {
 	@Autowired
-	JdbcTemplate jdbcTemplate;
-	
-	public void showJobCount() {
-		Integer count = jdbcTemplate.queryForObject("select count(*) from jobs", Integer.class);
-		System.out.println("Count = " + count);
-	}
-	
-//	@Autowired
-//	JobsManager jobs;
+	JobsManager jobs;
 	
 //	@Autowired
 //	SJIDemo sjiDemo;
@@ -27,21 +19,26 @@ public class TestJDBC implements CommandLineRunner {
 	}
 	
 	public void run(String... args) {
-		showJobCount();
+		// jobs.showJobCount();
 		
 		// jobs.updateMinSalary();
-		// jobs.listJobsWithLambda();
+		
+		//jobs.listJobTitlesWithJava7();
+		//jobs.listJobTitles();
+		
+		 // jobs.listJobsWithLambda();
 		 // jobs.showAverageSalary();
 		 //sjiDemo.add();
 		
 		// jobs.listJobTitles();
-//		try {
-//			System.out.println("Updating salaries!");
-//			jobs.updateTwo(1,12);
-//			System.out.println("Operation Completed!");
-//		} catch (Exception ex) {
-//			System.out.println("Operation Failed");
-//		}
+		
+		try {
+			System.out.println("Updating salaries!");
+			jobs.updateTwo(1,2);
+			System.out.println("Operation Completed!");
+		} catch (Exception ex) {
+			System.out.println("Operation Failed");
+		}
 	}
 
 }
